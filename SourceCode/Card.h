bc35c59c81e9e3409e1fd5393a42d7047e038fb1
Rakuter <2240132@ecc.ac.jp>
2024-11-16 15:37:58 +0900

    Update Card.h

--- SourceCode/Card.h
+++ SourceCode/Card.h
@@ -1,2 +1,32 @@
 #pragma once
 
+#include "all.h"
+#include "obj2d.h"
+#include <algorithm>
+using namespace std;
+
+class Card_INFO {
+    Sprite* spr;
+    const wchar_t* filePath;
+    VECTOR2         texPos;
+    VECTOR2         texSize;
+    VECTOR2         pivot;
+    float           offset;
+    float           radius;
+};
+
+class CardDate
+{
+private:
+    int SterCard[5] = { 1,2,3,4,5 };
+public:
+    CardDate();
+    ~CardDate();
+
+    void CardShuffle();
+};
+
+void Card_init();
+void Card_deinit();
+void Card_update();
+void Card_render();
