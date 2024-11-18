a781b79c93d91e3e7be87265470ce4595a418aff
Rakuter <2240132@ecc.ac.jp>
2024-11-18 18:15:35 +0900

    Update judge.cpp

--- SourceCode/judge.cpp
+++ SourceCode/judge.cpp
@@ -42,16 +42,15 @@ bool hitCheckCircle(VECTOR2 pos1, float r1, VECTOR2 pos2, float r2)
 bool hitCheck(OBJ2D* obj1, OBJ2D* obj2)
 {
 	return hitCheckCircle(
-		obj1->pos + obj1->offset, obj1->radius,
-		obj2->pos + obj2->offset, obj2->radius
+		obj1->pos, obj1->radius,
+		obj2->pos, obj2->radius
 	);
 }
-
 
 //  あたり判定
 void judge()
 {
-	// プレイヤーvs敵のあたり判定
+	// プレイヤーとカードのあたり判定
 	for (int i = 0; i < CARD_MAX; ++i)
 	{
 		if (!AnyCard[i].moveAlg)              continue;
