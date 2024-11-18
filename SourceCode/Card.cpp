f0af2cdeca4b8ca38ead46cd1b9b752580610776
Rakuter <2240132@ecc.ac.jp>
2024-11-18 18:22:21 +0900

    Update Card.cpp

--- SourceCode/Card.cpp
+++ SourceCode/Card.cpp
@@ -153,7 +153,7 @@ void Card_render()
 		primitive::circle(
 			AnyCard[i].pos+AnyCard[i].offset, AnyCard[i].radius,
 			{ 1.0f, 1.0f }, ToRadian(0),
-			{ 1.0f, 0.0f, 0.5f, 0.5f }
+			{ 0.0f, 0.3f, 0.5f, 0.5f }
 		);
 	}
 }
