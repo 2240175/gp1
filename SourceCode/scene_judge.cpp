f7874ce6ad74c2f14825ae8378109d8e7a9f06b1
Rakuter <2240132@ecc.ac.jp>
2024-11-19 15:21:41 +0900

    Update scene_judge.cpp

--- SourceCode/scene_judge.cpp
+++ SourceCode/scene_judge.cpp
@@ -1,18 +1,94 @@
 #include "Card.h"
 #include "scene_judge.h"
 
+
+Sprite* sprck;
+
+int judge_timer;
+int judge_state;
+int judge_time;
+
+extern OBJ2D AnyCard[CARD_MAX];
+extern bool UseCard[10];
+
+int NPCNUM;
+int PLAYERNUM;
+
 void judge_init()
 {
+	judge_timer = 0;
+	judge_state = 0;
+	judge_time = 0;
+	NPCNUM = 0;
+	PLAYERNUM = 0;
 }
 
 void judge_deinit()
 {
+	judge_timer = 0;
+	judge_state = 0;
+	judge_time = 0;
+
+	for (int i = 0; i < 10; i++) {
+		UseCard[i] = false;
+	}
+
+	safe_delete(sprck);
 }
 
 void judge_update()
 {
+	switch (judge_state) {
+	case 0:
+		sprck = sprite_load(L"./Data/Images/m.png");
+
+		judge_state++;
+		/*fallthrough*/
+	case 1:
+		GameLib::setBlendMode(Blender::BS_ALPHA);
+
+		judge_state++;
+		/*fallthrough*/
+	case 2:
+		debug::setString("JUDGE TIME");
+		debug::setString("judge_timer:%d", judge_timer/60);
+
+
+		debug::setString("NPC:%d", NPCNUM);
+		debug::setString("PLAYER:%d", PLAYERNUM);
+
+		for (int i = 0; i < 10; i++) {
+			if (UseCard[i] == true) {
+				if (i < 5) {
+					PLAYERNUM = i + 1;
+				}
+				else {
+					NPCNUM = i - 4;
+				}
+			}
+		}
+
+		if (PLAYERNUM < NPCNUM) {
+			debug::setString("NPC WIN");
+		}
+		else if (PLAYERNUM > NPCNUM) {
+			debug::setString("PLAYER WIN");
+		}
+		else {
+			debug::setString("!!!!!DRAW!!!!!");
+		}
+
+		if (TRG(0) & PAD_TRG1) {
+			nextScene = SCENE_GAME;
+		}
+
+		break;
+	}
+
+	judge_timer++;
 }
 
 void judge_render()
 {
+	sprite_render(sprck, 0, 0);
 }
