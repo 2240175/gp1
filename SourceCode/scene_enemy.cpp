c1e8888146d57172121d0a0332fe508b0bbbd4bc
Rakuter <2240132@ecc.ac.jp>
2024-11-20 00:00:46 +0900

    Update scene_enemy.cpp

--- SourceCode/scene_enemy.cpp
+++ SourceCode/scene_enemy.cpp
@@ -1,5 +1,8 @@
 #include "scene_enemy.h"
 
+Sprite* sprNPC[5];
+
+
 int enemy_timer;
 int enemy_state;
 
@@ -21,10 +24,21 @@ void enemy_deinit()
 	enemy_timer = 0;
 	enemy_state = 0;
 	NPCCard = 0;
+	for (int i = 0; i < 5; i++) {
+		safe_delete(sprNPC[i]);
+	}
 }
 
 void enemy_update()
 {
+	switch (enemy_state) {
+	case 0:
+		sprNPC[0] = sprite_load(L"./Data/Images/one.png");
+		sprNPC[1] = sprite_load(L"./Data/Images/two.png");
+		sprNPC[2] = sprite_load(L"./Data/Images/one.png");
+		sprNPC[3] = sprite_load(L"./Data/Images/one.png");
+		sprNPC[4] = sprite_load(L"./Data/Images/one.png");
+	}
 	if (Check == false) {
 		NPCCard = rand() % 5;
 		UseCard[NPCCard + 4] == true;
@@ -33,4 +47,21 @@ void enemy_update()
 
 void enemy_render()
 {
+	switch (NPCCard) {
+	case 0:
+		sprite_render(sprNPC[NPCCard], 300, 500);
+		break;
+	case 1:
+		sprite_render(sprNPC[NPCCard], 300, 500);
+		break;
+	case 2:
+		sprite_render(sprNPC[NPCCard], 300, 500);
+		break;
+	case 3:
+		sprite_render(sprNPC[NPCCard], 300, 500);
+		break;
+	case 4:
+		sprite_render(sprNPC[NPCCard], 300, 500);
+		break;
+	}
 }
