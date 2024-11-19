63aa95c0e06e78254fdfde8197ba8f9fc76ca041
Rakuter <2240132@ecc.ac.jp>
2024-11-19 14:07:01 +0900

    Create scene_enemy.cpp

---
+++ SourceCode/scene_enemy.cpp
@@ -0,0 +1,34 @@
+#include "scene_enemy.h"
+
+int enemy_timer;
+int enemy_state;
+
+int NPCCard;
+
+void enemy_init()
+{
+	enemy_timer = 0;
+	enemy_state = 0;
+	NPCCard = 0;
+}
+
+void enemy_deinit()
+{
+	enemy_timer = 0;
+	enemy_state = 0;
+	NPCCard = 0;
+}
+
+void enemy_update()
+{
+	switch (enemy_state)
+	{
+	case 0:
+		//‰Šúİ’è
+
+	}
+}
+
+void enemy_render()
+{
+}
