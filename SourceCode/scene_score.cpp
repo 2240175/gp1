26f3021576621c68cb39fc511b7944946e4381ad
Rakuter <2240132@ecc.ac.jp>
2024-11-20 11:12:24 +0900

    Update scene_score.cpp

--- SourceCode/scene_score.cpp
+++ SourceCode/scene_score.cpp
@@ -1,13 +1,21 @@
 #include "scene_score.h"
 
-void score_init() {
+void score_init()
+{
+
 }	
 
-void score_deinit() {
+void score_deinit() 
+{
+
 }
 
-void score_update() {
+void score_update() 
+{
+
 }	 
 
-void score_render() {
+void score_render()
+{
+
 }
