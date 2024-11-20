621bd59cbcf970ef55d99aa67bd2c24cc20bc1b8
Rakuter <2240132@ecc.ac.jp>
2024-11-21 01:08:25 +0900

    Update Aitem.cpp

--- SourceCode/Aitem.cpp
+++ SourceCode/Aitem.cpp
@@ -2,6 +2,8 @@
 
 bool AitemNum[10] = { false };
 
+Sprite* AitemPNG;
+
 extern int winner;
 extern int raund;
 extern int winraund;
@@ -9,29 +11,41 @@ extern int lossraund;
 
 extern int StarPiece;
 
+bool AitemNum[7] = { false };
+
 int money = rand() % 3;
+int AitemSelect = 0;
 
 void Aitem_init()
 {
-
+	AitemPNG = sprite_load(L"./Data/Images/Aitem/Aitem.png");
 }
 
 void Aitem_deinit()
 {
-
+	safe_delete(AitemPNG);
 }
 
 void Aitem_update()
 {
-
+	//アイテムに触れて押したらその数値を送る
 }
 
 void Aitem_render()
 {
-
+	sprite_render(AitemPNG, 0, 0);
 }
 
 void AitemBuy()
 {
-
+	switch (AitemSelect) {
+	case 0:
+		break;
+	case 1:
+		if (AitemNum[0] == false&& StarPiece>=3) {
+			AitemNum[0] = true;
+			StarPiece += -3;
+		}
+		break;
+	}
 }
