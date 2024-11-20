c807bac22b5aa3b9674b5aa2e320f65dae795c62
Rakuter <2240132@ecc.ac.jp>
2024-11-21 01:08:37 +0900

    Update scene_score.cpp

--- SourceCode/scene_score.cpp
+++ SourceCode/scene_score.cpp
@@ -3,7 +3,9 @@
 int score_timer;
 int score_state;
 
-Sprite* sprScore;
+extern int Game_Winner;
+
+Sprite* sprScore[4];
 
 void score_init()
 {
@@ -13,14 +15,19 @@ void score_init()
 
 void score_deinit() 
 {
-	safe_delete(sprScore);
+	for (int i = 0; i < 4; i++) {
+		safe_delete(sprScore[i]);
+	}
 }
 
 void score_update() 
 {
 	switch (score_state) {
 	case 0:
-		sprScore = sprite_load(L"./Data/Images/m.jpg");
+		sprScore[0] = sprite_load(L"./Data/Images/m.jpg");
+		sprScore[1] = sprite_load(L"./Data/Images/win.png");
+		sprScore[2] = sprite_load(L"./Data/Images/loss.png");
+		sprScore[3] = sprite_load(L"./Data/Images/error.png");
 		score_state++;
 		/*fallthrough*/
 	case 1:
@@ -29,11 +36,23 @@ void score_update()
 		score_state++;
 		/*fallthrough*/
 	case 2:
+
 		break;
 	}
 }	 
 
 void score_render()
 {
-	sprite_render(sprScore, 0, 0);
+	sprite_render(sprScore[0], 0, 0);
+	switch (Game_Winner) {
+	case 0:
+		sprite_render(sprScore[3], 0, 250);
+		break;
+	case 1:
+		sprite_render(sprScore[1], 0, 250);
+		break;
+	case 2:
+		sprite_render(sprScore[2], 0, 250);
+		break;
+	}
 }
