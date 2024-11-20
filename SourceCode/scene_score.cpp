d288a6eed81cee4876088ef689afd25bf5f1fc1c
Rakuter <2240132@ecc.ac.jp>
2024-11-20 11:57:22 +0900

    Update scene_score.cpp

--- SourceCode/scene_score.cpp
+++ SourceCode/scene_score.cpp
@@ -1,21 +1,39 @@
 #include "scene_score.h"
 
+int score_timer;
+int score_state;
+
+Sprite* sprScore;
+
 void score_init()
 {
-
+	score_timer = 0;
+	score_state = 0;
 }	
 
 void score_deinit() 
 {
-
+	safe_delete(sprScore);
 }
 
 void score_update() 
 {
+	switch (score_state) {
+	case 0:
+		sprScore = sprite_load(L"./Data/Images/m.jpg");
+		score_state++;
+		/*fallthrough*/
+	case 1:
+		GameLib::setBlendMode(Blender::BS_ALPHA);
 
+		score_state++;
+		/*fallthrough*/
+	case 2:
+		break;
+	}
 }	 
 
 void score_render()
 {
-
+	sprite_render(sprScore, 0, 0);
 }
