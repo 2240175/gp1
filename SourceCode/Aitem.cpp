931a5b910984593de96500bac0ea63acc58216fd
Rakuter <2240132@ecc.ac.jp>
2024-11-20 13:26:33 +0900

    Create Aitem.cpp

---
+++ SourceCode/Aitem.cpp
@@ -0,0 +1,37 @@
+#include "Aitem.h"
+
+bool AitemNum[10] = { false };
+
+extern int winner;
+extern int raund;
+extern int winraund;
+extern int lossraund;
+
+extern int StarPiece;
+
+int money = rand() % 3;
+
+void Aitem_init()
+{
+
+}
+
+void Aitem_deinit()
+{
+
+}
+
+void Aitem_update()
+{
+
+}
+
+void Aitem_render()
+{
+
+}
+
+void AitemBuy()
+{
+
+}
