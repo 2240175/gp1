fc684ec0567baccebc36255e099e687e9357b677
Rakuter <2240132@ecc.ac.jp>
2024-11-27 23:45:40 +0900

    Update end.cpp

--- SourceCode/end.cpp
+++ SourceCode/end.cpp
@@ -35,19 +35,33 @@ void end_update()
 		/*fallthrough*/
 
 	case 2:
-		if (end_timer > 300) {
+		if (end_timer > 7380 || end_timer > 30 && GetAsyncKeyState(VK_LBUTTON) & 0x8000) {
 			nextScene = SCENE_TITLE;
 		}
 		break;
 	}
 
-	debug::setString("end_timer:%d",end_timer);
+	
 	end_timer++;
 }
 
 void end_render()
 {
-	sprite_render(sprEnd[0], 0, 0);
-	sprite_render(sprEnd[1], 0, 0);
-	sprite_render(sprEnd[2], 0, 0);
+	GameLib::clear(0.0f, 0.0f, 0.0f);
+
+		
+	sprite_render(sprEnd[0], 0, ((-end_timer)+2000)*0.5f);
+	
+	sprite_render(sprEnd[1], 0, ((-end_timer) + 3280) * 0.5f);
+	
+	sprite_render(sprEnd[2], 0, ((-end_timer) + 4560) * 0.5f);
+
+	if (((-end_timer + 2400) * 0.5f) == 0) {
+		sprite_render(sprEnd[2], 0, 0);
+	}
+		
+	
+	debug::display(1.0f, 1.0f, 1.0f, 1.5f, 1.2f);
+	debug::setString("end_timer:%d", end_timer);
+	font::textOut(4, "Click Skip", VECTOR2(1070, 700), VECTOR2(1.0f, 1.0f), VECTOR4(0.7f, 0.7f, 0.7f, 0.7f));
 }
